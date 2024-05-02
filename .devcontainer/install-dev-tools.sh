37e65bc09abf35fcf94126e059e82a48f44b2810
Alan Wang <krantas@gmail.com>
2024-05-02 14:54:57 +0800

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -4,3 +4,4 @@ apt install software-properties-common wget curl python3-dev python3-pip python3
 apt autoremove -y
 apt clean
 python3 -m pip install --upgrade pip
+pip3 install --user -r .devcontainer/requirements.txt
